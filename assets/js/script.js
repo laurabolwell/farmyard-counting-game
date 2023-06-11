d4827e03b9c8f73eb7e6e001c34a70279f87797b
laurabolwell <laurabolwell@hotmail.com>
2023-06-11 11:29:01 +0000

    Add giveFeedback empty function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let game = {
     questionCount: 0,
     playerAnswer: 0,
     choices: ["button-1", "button-2", "button-3", "button-4", "button-5"]
-}
+};
 
 function playGame() {
     game.correctAnswers = 0;
@@ -15,32 +15,37 @@ function playGame() {
     for (let i=0; i<10; i++) {
         playerTurn();
     };   
-}
+};
 
 function playerTurn() {
     newQuestion()
     displayQuestion()
     checkAnswer()
+    giveFeedback()
     showScore()
-}
+};
 
 function newQuestion() {
     //??clear player answer?
     //selects random number from 1-5 and assigns it to game.currentQuestion
     game.currentQuestion = Math.floor((Math.random() * 5)+1)
-}
+};
 
 function displayQuestion() {
 //takes value selected in newQuestion and displays the required number of pictures
-    }
+    };
 
 function checkAnswer() {
     //listens for user answer and checks if it is correct
     // if yes increment game.correctAnswers
     // if no increment game.incorrectAnswers
-}
+};
+
+function giveFeedback() {
+
+};
 
 function showScore() {
     document.getElementById("correct").innerText = game.correctAnswers;
     document.getElementById("incorrect").innerText = game.incorrectAnswers;
-}
+};
